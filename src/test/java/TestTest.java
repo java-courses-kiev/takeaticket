72ee7676f02df25a9501123f6eda5146191f2f50
Dmitry Sen <dmitryssen@gmail.com>
2013-12-15 11:50:44 +0200

    TAK-10. added sourceRoot and testSourceRoot to pom.xml

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestTest {
+
+  /**
+   * This method tests assertTrue
+   */
+  @Test
+  public void testTest1(){
+    assertTrue(true);
+  }
+}
