3b8a5eca3868e95e4e948d7cdca1b288d5e5d9b9
Dmitry Sen <dmitryssen@gmail.com>
2013-12-15 12:19:05 +0200

    TAK-10. Added test example

--- src/main/webapp/java/ua/kiev/javacourses/servlets/TestServlet.java
+++ src/main/webapp/java/ua/kiev/javacourses/servlets/TestServlet.java
@@ -1,4 +1,4 @@
-package ua.kiev.javacourses;
+package ua.kiev.javacourses.servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -6,16 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created with IntelliJ IDEA.
- * User: dmitry
- * Date: 14.12.13
- * Time: 22:45
- * To change this template use File | Settings | File Templates.
- */
 public class TestServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
     httpServletResponse.getWriter().print("hello");
+  }
+
+  public boolean servletHelperMethod(){
+    return true;
   }
 }
--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import ua.kiev.javacourses.servlets.TestServlet;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class TestTest {
@@ -11,4 +13,15 @@ public class TestTest {
   public void testTest1(){
     assertTrue(true);
   }
+
+  /**
+   * This method tests access to the java packages and classes
+   */
+  @Test
+  public void testTest2(){
+    TestServlet ts = new TestServlet();
+    assertTrue(ts.servletHelperMethod());
+    assertNotNull(ts);
+  }
+
 }
